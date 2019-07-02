0bfad587f28398d982f15ea86d21c5fc894f5b05
chenli <596919197@qq.com>
2019-07-02 09:07:53 +0800

    记事翻页按钮

---
+++ js/page.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    var pageSize=11;
+    $("#nextOpt").click(function () {
+        var i=$("#nextOpt").attr('name');
+        if (i>=pageSize){
+            i=0;
+        }
+        i++;
+        $("#jishi").attr('src',"./powerview/幻灯片"+i+".jpg");
+        $("#nextOpt").attr('name',i);
+    });
+
+    $("#frontOpt").click(function () {
+        var i=$("#nextOpt").attr('name');
+            if (i<=1){
+                i=pageSize+1;
+            }
+            i--;
+            $("#jishi").attr('src',"./powerview/幻灯片"+i+".jpg");
+            $("#nextOpt").attr('name',i);
+    });
+
+
+
+});
