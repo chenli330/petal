cb5332fac5823a9cbf6c6bf3c84423e4a09923e6
chenli <596919197@qq.com>
2019-07-03 11:30:34 +0800

    跨域测试

--- js/page.js
+++ js/page.js
@@ -1,34 +1,40 @@
 $(document).ready(function () {
-    var pageSize=11;
+    var pageSize = 11;
     $("#nextOpt").click(function () {
-        var i=$("#nextOpt").attr('name');
-        if (i>=pageSize){
-            i=0;
+        var i = $("#nextOpt").attr('name');
+        if (i >= pageSize) {
+            i = 0;
         }
         i++;
-        $("#jishi").attr('src',"./powerview/幻灯片"+i+".jpg");
-        $("#nextOpt").attr('name',i);
+        $("#jishi").attr('src', "./powerview/幻灯片" + i + ".jpg");
+        $("#nextOpt").attr('name', i);
     });
 
     $("#frontOpt").click(function () {
-        var i=$("#nextOpt").attr('name');
-            if (i<=1){
-                i=pageSize+1;
-            }
-            i--;
-            $("#jishi").attr('src',"./powerview/幻灯片"+i+".jpg");
-            $("#nextOpt").attr('name',i);
+        var i = $("#nextOpt").attr('name');
+        if (i <= 1) {
+            i = pageSize + 1;
+        }
+        i--;
+        $("#jishi").attr('src', "./powerview/幻灯片" + i + ".jpg");
+        $("#nextOpt").attr('name', i);
     });
 
+    $("#frontWish").click(function () {
+        alert("待完善");
+    });
+    $("#nextWish").click(function () {
+        alert("待完善");
+    });
+
+
     $("#addWishClick").click(function () {
-        var value =$("#input").val();
+        var value = $("#input").val();
         if (value) {
             createItem(value);
             $("#input").val('');
         }
     });
-
-
 
 
 });
